8e953194d5c27015089eaaae8a155396e5790c1c
viveroa2291 <viveroa2291@uwec.edu>
2023-01-21 17:18:00 -0600

    Some styling and edits
    Did some styling and edits to my about page.

--- about.php
+++ about.php
@@ -22,17 +22,19 @@
     <h2 class="about-header">About Page</h2>
     <hr class="about-hr">
     <section class="about-section">
+        <h2>Your brand new home for a Dictionary</h2>
+        <h5>New terms and new categories to distinguish them.</h5>
         <div class="about-image">
             <div class="picture">
                 <img src="images/dock.jpeg" alt="Picture of Adan">    
             </div>
             <div class="about-me">
                 <article id="about-me">
-                    <h2>About Me</h2>
+                    <h2 id="adan">About Me (The Creator)</h2>
                     <hr>
                     <p>&nbsp;&nbsp;&nbsp;&nbsp; I am a 25 year old with a Bachelors degree in Computer Science from the <a href="https://www.uwec.edu/">University of Wisconsin Eau Claire</a>. I currently reside in Madison, Wisconsin, but was raised in Mount Prospect, Illinois, a northwest suburb of Chicago.</p>
-                    <p>Growing up, I participated in soccer, wrestling, and gymnastics. My hobbies now include going to the gym, programming, going on hikes, playing video games.</p>
-                    <p>Aside from programming, I like to go for photo shoots and take pictures of nature and cities. I also enjoy video editing and have plans to upload content on <a href="https://rumble.com/user/adxadan">Rumble</a>.</p>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; Growing up, I participated in soccer, wrestling, and gymnastics. My hobbies now include going to the gym, programming, going on hikes, playing video games.</p>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp; Aside from programming, I like to go for photo shoots and take pictures of nature and cities. I also enjoy video editing and have plans to upload content on <a href="https://rumble.com/user/adxadan">Rumble</a>.</p>
                     <h4>Computer Science Background</h4>
                     <hr>
                     <p>&ensp;&ensp;&nbsp;&nbsp; I began programming back in high school starting off with IOS app development working with the languages Swift. I made few school projects at the time such as tic-tac-toe and a University match search engine.</p>
@@ -55,7 +57,7 @@
             <article id="about">
                 <h2>About Adan's Dictionary</h2>
                 <hr>
-                <p>&nbsp;&nbsp;&nbsp;&nbsp; This page was created by me (Adan Vivero) as a way of adding definitions to words I did not know. Everyday I would encounter words that I did not know the meaning or definition to and decided to make a website, something I enjoy doing and write down the definitions to the words.</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; This page was created by me <a href="#adan">(Adan Vivero)</a> as a way of adding definitions to words I did not know. Everyday I would encounter words that I did not know the meaning or definition to and decided to make a website, something I enjoy doing and write down the definitions to the words.</p>
                 <p>&nbsp;&nbsp;&nbsp;&nbsp; This site is currently in its beta form, but I plan to implement the search bar and categorize the definition by the fields in which they are attributed to.</p>
                 <p>&nbsp;&nbsp;&nbsp;&nbsp; What I hope to accomplish with this website is opening up my vocabulary but also sharing these definitions with others as I believe we should all have a broader vocabulary to words. </p>
                 <p>This website was created by using 
@@ -69,6 +71,12 @@
             </article>
         </div>
     </section>
-  
+    <section class="blocks">
+        <div class="block1"></div>
+        <div class="block2"></div>
+        <div class="block3"></div>
+        <div class="block4"></div>
+    </section>
+    
 </body>
 </html>
